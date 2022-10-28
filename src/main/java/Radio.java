99ff3dcfb68c17d691ff53a7e6f0c3bdf44ad304
Diana Vakhitova <dianaum0809@gmail.com>
2022-10-28 13:45:51 +0300

    new radio

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,60 +1,88 @@
 public class Radio {
     private int currentRadioStation;
+    private int minStation;
+    private int maxStation;
+
+    public Radio() {
+        this.maxStation = 9;
+    }
+
+    public Radio(int stationsAmount) {
+        this.maxStation = stationsAmount - 1;
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
     }
 
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
     public void setCurrentRadioStation(int currentRadioStation) {
-        if (currentRadioStation < 0) {
+        if (currentRadioStation < minStation) {
             return;
         }
-        if (currentRadioStation > 9) {
-            currentRadioStation = 9;
+        if (currentRadioStation > maxStation) {
+            currentRadioStation = maxStation;
         }
         this.currentRadioStation = currentRadioStation;
     }
 
     public void next() {
-        if (currentRadioStation < 9) {
+        if (currentRadioStation < maxStation) {
             currentRadioStation++;
         } else {
-            currentRadioStation = 0;
+            currentRadioStation = minStation;
         }
     }
 
     public void prev() {
-        if (currentRadioStation > 0) {
+        if (currentRadioStation > minStation) {
             currentRadioStation--;
         } else {
-            currentRadioStation = 9;
+            currentRadioStation = maxStation;
         }
     }
 
     private int volume;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
 
     public int getVolume() {
         return volume;
     }
 
     public void setVolume(int volume) {
-        if (volume < 0) {
+        if (volume < minVolume) {
             return;
         }
-        if (volume > 10) {
-            volume = 10;
+        if (volume > maxVolume) {
+            volume = maxVolume;
         }
         this.volume = volume;
     }
 
     public void increaseVolume() {
-        if (volume < 10) {
+        if (volume < maxVolume) {
             volume++;
         }
     }
 
     public void decreaseVolume() {
-        if (volume > 0) {
+        if (volume > minVolume) {
             volume--;
         }
     }
